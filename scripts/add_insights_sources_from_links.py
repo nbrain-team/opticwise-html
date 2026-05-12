ca8b0a871783683a09ce244785177429d9381d4b
Bill Douglas <billdouglas@Bills-2024-MacBook-Pro.local>
2026-05-12 16:40:18 -0600

    auto: file changes 2026-05-12 16:40:18

--- scripts/add_insights_sources_from_links.py
+++ scripts/add_insights_sources_from_links.py
@@ -41,7 +41,7 @@ _NAMED_RULES_RAW: list[tuple[str, str, str]] = [
     (r"j\.?\s*p\.?\s*morgan(?:\s+chase|\s+research)?", "https://www.jpmorganchase.com/", "JPMorgan Chase"),
     (r"morgan\s+stanley", "https://www.morganstanley.com/", "Morgan Stanley"),
     (r"bain\s*&\s*company", "https://www.bain.com/", "Bain & Company"),
-    (r"p\.?\s*w\.?\s*c\.?", "https://www.pwc.com/", "PwC"),
+    (r"ernst\s*&\s*young", "https://www.ey.com/", "Ernst & Young"),
     (r"wall\s+street\s+journal|\bwsj\b", "https://www.wsj.com/", "The Wall Street Journal"),
     (r"fast\s+company", "https://www.fastcompany.com/", "Fast Company"),
     (r"digital\s+realty", "https://www.digitalrealty.com/", "Digital Realty"),
